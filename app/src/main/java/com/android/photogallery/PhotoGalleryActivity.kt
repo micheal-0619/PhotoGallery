a995bcd4dc478396a543ce00efee97ecdcb62ac8
Michael_0619 <any_leon@163.com>
2023-07-03 10:28:08 +0800

    [RecyclerView][Fragment]init recyclerView and fragment

--- app/src/main/java/com/android/photogallery/PhotoGalleryActivity.kt
+++ app/src/main/java/com/android/photogallery/PhotoGalleryActivity.kt
@@ -2,10 +2,20 @@ package com.android.photogallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.photogallery.fragment.PhotoGalleryFragment
 
 class PhotoGalleryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_gallery)
+
+        val isFragmentContainerEmpty = savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance())
+                .commit()
+        }
+
     }
 }

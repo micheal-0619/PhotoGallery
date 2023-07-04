a99d6fd5c75103a18eca5ac1114eaa554a2540b2
Michael_0619 <any_leon@163.com>
2023-07-04 10:29:08 +0800

    [Handler]Create and send a message

--- app/src/main/java/com/android/photogallery/thread/ThumbnailDownloader.kt
+++ app/src/main/java/com/android/photogallery/thread/ThumbnailDownloader.kt
@@ -1,17 +1,43 @@
 package com.android.photogallery.thread
 
+import android.annotation.SuppressLint
+import android.os.Handler
 import android.os.HandlerThread
+import android.os.Message
 import android.util.Log
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
+import com.android.photogallery.pattern.FlickrFetchr
+import java.util.concurrent.ConcurrentHashMap
 
 
 private const val TAG = "ThumbnailDownloader"
+private const val MESSAGE_DOWNLOAD = 0
 
 class ThumbnailDownloader<in T> : HandlerThread(TAG), LifecycleObserver {
 
     private var hasQuit = false
+
+    private lateinit var requestHandler: Handler
+    private val requestMap = ConcurrentHashMap<T, String>()
+    private val flickrFetchr = FlickrFetchr()
+
+    @Suppress("UNCHECKED_CAST")
+    @SuppressLint("HandlerLeak")
+    override fun onLooperPrepared() {
+        super.onLooperPrepared()
+        requestHandler = object : Handler() {
+            override fun handleMessage(msg: Message) {
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    val target = msg.obj as T
+                    Log.i(TAG, "Got a request for URL: ${requestMap[target]} ")
+                    handleRequest(target)
+                }
+            }
+        }
+    }
+
     override fun quit(): Boolean {
         hasQuit = true
         return super.quit()
@@ -32,5 +58,13 @@ class ThumbnailDownloader<in T> : HandlerThread(TAG), LifecycleObserver {
 
     fun queueThumbnail(target: T, url: String) {
         Log.i(TAG, "Got a URL: $url ")
+
+        requestMap[target] = url
+        requestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget()
+    }
+
+    private fun handleRequest(target: T) {
+        val url = requestMap[target] ?: return
+        val bitmap = flickrFetchr.fetchPhoto(url) ?: return
     }
 }

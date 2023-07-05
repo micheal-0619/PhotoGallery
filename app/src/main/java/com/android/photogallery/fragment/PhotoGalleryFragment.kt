fb06017fd34463f77440374a87479786d697410a
Michael_0619 <any_leon@163.com>
2023-07-05 16:09:24 +0800

    [Intent][click]Using implicit Intent, click on the image to open the browser and redirect to the corresponding website

--- app/src/main/java/com/android/photogallery/fragment/PhotoGalleryFragment.kt
+++ app/src/main/java/com/android/photogallery/fragment/PhotoGalleryFragment.kt
@@ -1,5 +1,6 @@
 package com.android.photogallery.fragment
 
+import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
@@ -178,10 +179,25 @@ class PhotoGalleryFragment : VisibleFragment() {
 
     }
 
-    private class PhotoHolder(private val itemImageView: ImageView) :
-        RecyclerView.ViewHolder(itemImageView) {
+    private inner class PhotoHolder(private val itemImageView: ImageView) :
+        RecyclerView.ViewHolder(itemImageView), View.OnClickListener {
+
+        private lateinit var galleryItem: GalleryItem
+
+        init {
+            itemView.setOnClickListener(this)
+        }
 
         val bindDrawable: (Drawable) -> Unit = itemImageView::setImageDrawable
+
+        fun bindGalleryItem(item: GalleryItem) {
+            galleryItem = item
+        }
+
+        override fun onClick(view: View) {
+            val intent = Intent(Intent.ACTION_VIEW, galleryItem.photoPageUri)
+            startActivity(intent)
+        }
     }
 
     private inner class PhotoAdapter(private val galleryItems: List<GalleryItem>) :
@@ -197,6 +213,7 @@ class PhotoGalleryFragment : VisibleFragment() {
 
         override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
             val galleryItem = galleryItems[position]
+            holder.bindGalleryItem(galleryItem) //绑定GalleryItem
             val placeholder: Drawable = ContextCompat.getDrawable(
                 requireContext(), R.drawable.ic_default
             ) ?: ColorDrawable()

dc182b3b1218748aa28be88554c6a2ade203f342
Michael_0619 <any_leon@163.com>
2023-07-05 14:20:28 +0800

    [Broadcast]Send a broadcast with permissions

--- app/src/main/java/com/android/photogallery/worker/PollWorker.kt
+++ app/src/main/java/com/android/photogallery/worker/PollWorker.kt
@@ -3,6 +3,7 @@ package com.android.photogallery.worker
 import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -65,8 +66,17 @@ class PollWorker(val context: Context, val workerParams: WorkerParameters) :
                 .build()
             val notificationManager = NotificationManagerCompat.from(context)
             notificationManager.notify(0, notification)
+
+            //发送带有权限PERM_PRIVATE的broadcast
+            context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION), PERM_PRIVATE)
         }
 
         return Result.success()
+    }
+
+    companion object {
+        const val ACTION_SHOW_NOTIFICATION =
+            "com.android.photogallery.SHOW_NOTIFICATION"
+        const val PERM_PRIVATE = "com.android.photogallery.PRIVATE"
     }
 }

a65214460804e02ea9d93b46a3bc8748a12859a0
Michael_0619 <any_leon@163.com>
2023-07-05 17:16:36 +0800

    [WebView]Add Progress bar

--- app/src/main/java/com/android/photogallery/fragment/PhotoPageFragment.kt
+++ app/src/main/java/com/android/photogallery/fragment/PhotoPageFragment.kt
@@ -7,8 +7,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 import com.android.photogallery.R
 
 private const val ARG_URI = "photo_page_url"
@@ -18,6 +21,7 @@ class PhotoPageFragment : VisibleFragment() {
 
     private lateinit var uri: Uri
     private lateinit var webView: WebView
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +37,27 @@ class PhotoPageFragment : VisibleFragment() {
         val view = inflater.inflate(R.layout.fragment_photo_page, container, false)
         webView = view.findViewById(R.id.web_view)
 
+        progressBar = view.findViewById(R.id.progress_bar)
+        progressBar.max = 100
+
         /*步骤1.加载URL*/
         webView.settings.javaScriptEnabled = true
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(webView: WebView, newProgress: Int) {
+                if (newProgress == 100) {
+                    progressBar.visibility = View.GONE
+                } else {
+                    progressBar.visibility = View.VISIBLE
+                    progressBar.progress = newProgress
+                }
+            }
+
+            override fun onReceivedTitle(view: WebView?, title: String?) {
+                (activity as AppCompatActivity).supportActionBar?.subtitle = title
+            }
+        }
+
         webView.webViewClient = WebViewClient()
         webView.loadUrl(uri.toString())
         Log.d(TAG, "$uri")
